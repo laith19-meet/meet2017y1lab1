f1632f07f59f5462ebf2d27763e231ed59e74a30
laith19-meet <laith19@meet.mit.edu>
2017-07-23 19:03:06 +0300

    Stopping here

---
+++ laith_turtle.py
@@ -0,0 +1,24 @@
+import turtle
+
+
+# Everything that comes after the # is a
+
+# cooment .
+
+# It is a note to the person reading the code
+
+#the computer ignores it .
+
+#write your code below here...
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+turtle.goto(
+
+# ... and end it before the next line.
+
+turtle.mainloop()
